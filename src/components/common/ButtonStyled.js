97cfc484181d96789c80766d0e4602ba6cd070f2
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-04-22 21:29:01 +0100

    Removed the font

--- src/components/common/ButtonStyled.js
+++ src/components/common/ButtonStyled.js
@@ -2,6 +2,6 @@ import styled from "styled-components"
 import tw from "tailwind.macro"
 
 export const ButtonStyle = styled.button`
-  ${tw`py-1 px-2 mr-2 text-sm font-semibold text-white rounded-full`};
+  ${tw`py-1 px-2 mr-2 text-sm font-semibold text-white text-center rounded-full`};
   ${(props) => (props.primary ? tw`bg-cyan-dark` : tw`bg-cyan-darker`)};
 `
